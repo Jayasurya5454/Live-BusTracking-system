f138066f307e04d0268a7c53f1410e368e71b6d1
Harsine07 <harsineku.22aim@kongu.edu>
2024-04-12 23:45:10 +0530

    login

--- BusTrack/components/Admin/AdminLogin/AdminLoginStyles.js
+++ BusTrack/components/Admin/AdminLogin/AdminLoginStyles.js
@@ -1,4 +1,4 @@
-// AdminLoginStyles.js
+// DriverLoginStyles.js
 
 import { StyleSheet } from 'react-native';
 
@@ -7,46 +7,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5', 
+    backgroundColor: '#f5f5f5', // Background color for the entire screen
   },
   inputContainer: {
-<<<<<<< HEAD
-    height: 500,
-    width:450,
-    borderWidth: 1,
-    borderColor: 'black',
-   justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-    borderRadius: 5,
-    overflow: 'hidden', 
-    elevation: 10, // Use elevation to create a shadow effect
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-  },
-
-  title: {
-    fontSize: 38, 
-    fontWeight: 'bold', 
-    color:'#3c009d',
-    marginBottom: 45,
-
-  },
-
- 
-
-  input: {
-    width: '80%',
-    height: 60,
-    borderBottomWidth: 1, // Add bottom border
-    borderBottomColor: 'black', 
-   
-    marginVertical: 10,
-    paddingHorizontal: 10,
-    fontSize: 16,
-=======
     width: '80%',
     backgroundColor: '#fff', // Background color for the input container
     padding: 20,
@@ -71,16 +34,9 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 10,
     paddingHorizontal: 10,
->>>>>>> b2776eeab94cbe422775747ba11121b768efe9ea
   },
   buttonContainer: {
-<<<<<<< HEAD
-    width: '45%',
-    
-    marginTop: 50,
-=======
     alignItems: 'center',
->>>>>>> b2776eeab94cbe422775747ba11121b768efe9ea
   },
   button: {
     backgroundColor: '#007bff',

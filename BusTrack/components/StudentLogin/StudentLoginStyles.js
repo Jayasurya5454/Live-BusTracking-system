bccec2969fa31492eb42f39dcddad0ea2f82bee5
Harsine07 <harsineku.22aim@kongu.edu>
2024-04-15 11:09:56 +0530

    form css

--- BusTrack/components/StudentLogin/StudentLoginStyles.js
+++ BusTrack/components/StudentLogin/StudentLoginStyles.js
@@ -7,7 +7,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: 'lightgrey',
   },
   inputContainer: {
     width: '80%',
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
     textAlign: 'center',
-    color: '#3c009d',
+    color: '#004080',
   },
   input: {
     height: 40,
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   button: {
-    backgroundColor: '#007bff',
+    backgroundColor: '#2596be',
     paddingVertical: 12,
     borderRadius: 25,
     width: '50%', 

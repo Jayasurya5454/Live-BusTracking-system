ed71841b21b7c048adbf0e8d5a27f6877ba3f634
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2024-04-17 01:09:56 +0530

    facult details shown

--- BusTrack/components/FacultyLogin/FacultyInfoScreen.js
+++ BusTrack/components/FacultyLogin/FacultyInfoScreen.js
@@ -8,8 +8,14 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 import { ref, get } from 'firebase/database';
 import { db } from '../../firebaseConfig';
 
-
-
+const attributeNames = {
+  bus_no: 'Bus Number',
+  bus_stop: 'Bus Stop',
+  driver_name: 'Driver Name',
+  email: 'Email',
+  name: 'Name',
+  phone_no: 'Phone Number',
+};
 
 const FacultyInfoScreen = () => {
   const navigation = useNavigation();
@@ -22,7 +28,7 @@ const FacultyInfoScreen = () => {
   const [busStops, setBusStops] = useState([]);
   const [locations, setLocations] = useState([]);
   const route = useRoute();
-  const { name ,busNumber} = route.params || {};
+  const { name, busNumber } = route.params || {};
 
   const fadeInAnim = useRef(new Animated.Value(0)).current;
   const userDataAnim = useRef(new Animated.Value(0)).current;
@@ -36,13 +42,12 @@ const FacultyInfoScreen = () => {
   }, [fadeInAnim]);
 
   useEffect(() => {
-    // Fetch bus route based on the passed busNumber
-    fetchBusRouteForBusNumber(busNumber); // Use the busNumber passed from navigation params
+    fetchBusRouteForBusNumber(busNumber);
   }, []);
 
   const fetchBusRouteForBusNumber = async (busNumber) => {
     try {
-      const busRoutesRef = ref(db, `bus_routes/${busNumber}`); // Use template literals to include the dynamic busNumber
+      const busRoutesRef = ref(db, `bus_routes/${busNumber}`);
       const snapshot = await get(busRoutesRef);
       if (snapshot.exists()) {
         const routeData = snapshot.val();
@@ -61,14 +66,13 @@ const FacultyInfoScreen = () => {
 
         setBusStops(stops);
         setLocations(locs);
-        setBusPosition(stops[0]); // Set initial bus position
+        setBusPosition(stops[0]);
         if (mapRef.current) {
-          // Set initial region centered around the first bus stop
           mapRef.current.animateToRegion({
             latitude: stops[0].latitude,
             longitude: stops[0].longitude,
-            latitudeDelta: 0.05, // Adjust this value to change the zoom level
-            longitudeDelta: 0.05 // Adjust this value to change the zoom level
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05
           });
         }
       } else {
@@ -81,37 +85,35 @@ const FacultyInfoScreen = () => {
 
   const toggleMapVisibility = () => {
     setIsMapVisible(!isMapVisible);
-    setShowUserData(false); // Hide user data when map is toggled
+    setShowUserData(false);
   };
 
-  const toggleStudentDataVisibility = async () => {
+  const toggleFacultyDataVisibility = async () => {
     if (!userData) {
-      const studentRef = ref(db, 'staff');
-      get(studentRef)
+      const staffRef = ref(db, 'staff');
+      get(staffRef)
         .then(snapshot => {
           if (snapshot.exists()) {
             const data = snapshot.val();
-            const user = Object.values(data).find(staff => staff.bus_no === busNumber);
-            if (user) {
-              // Preprocess user data attribute names
+            const faculty = Object.values(data).find(staff => staff.bus_no === busNumber);
+            if (faculty) {
               const processedUserData = {};
-              Object.entries(user).forEach(([key, value]) => {
+              Object.entries(faculty).forEach(([key, value]) => {
                 const processedKey = attributeNames[key] || key;
                 processedUserData[processedKey] = value;
               });
               setUserData(processedUserData);
 
-              // Animate user data view
               Animated.timing(userDataAnim, {
                 toValue: 1,
                 duration: 1000,
                 useNativeDriver: true,
               }).start();
             } else {
-              console.log('User not found');
+              console.log('Faculty not found');
             }
           } else {
-            console.log('No data found at the "students" node.');
+            console.log('No data found at the "staff" node.');
           }
         })
         .catch(error => {
@@ -122,90 +124,69 @@ const FacultyInfoScreen = () => {
     setShowUserData(!showUserData);
   };
 
-  // Continuous movement of the bus
   useEffect(() => {
     if (busStops.length > 0) {
       const interval = setInterval(() => {
-        // Check if the bus is at the final destination
         if (busPositionIndex === busStops.length - 1) {
-          clearInterval(interval); // Stop the interval
+          clearInterval(interval);
           return;
         }
 
-        // Calculate the next stop index
         const nextStopIndex = busPositionIndex + 1;
-
-        // Calculate the distance between current and next stop
         const distance = calculateDistance(
           busStops[busPositionIndex].latitude,
           busStops[busPositionIndex].longitude,
           busStops[nextStopIndex].latitude,
           busStops[nextStopIndex].longitude
         );
-
-        // Calculate the number of steps based on distance
-        const numSteps = Math.ceil(distance / 100); // Adjust the step size as needed
-
-        // Calculate the step size for latitude and longitude
+        const numSteps = Math.ceil(distance / 100);
         const stepLat = (busStops[nextStopIndex].latitude - busStops[busPositionIndex].latitude) / numSteps;
         const stepLng = (busStops[nextStopIndex].longitude - busStops[busPositionIndex].longitude) / numSteps;
 
-        // Animate the bus marker to move along the polyline
         let i = 0;
         const animationInterval = setInterval(() => {
-          // Calculate the new position
           const newPosition = {
             latitude: busStops[busPositionIndex].latitude + i * stepLat,
             longitude: busStops[busPositionIndex].longitude + i * stepLng
           };
 
-          // Update the position of the bus marker
           setBusPosition(newPosition);
-
-          // Increment the step counter
           i++;
 
-          // Check if reached the next stop
           if (i > numSteps) {
-            // Move to the next stop
             setBusPositionIndex(nextStopIndex);
-
-            // Stop the animation
             clearInterval(animationInterval);
-
-            // Call useEffect again to start animation for the next segment
             setTimeout(() => {
               setBusPositionIndex(nextStopIndex);
-            }, 3000); // Delay before moving to the next stop (in milliseconds)
+            }, 3000);
           }
-        }, 30); // Interval for smooth animation (in milliseconds)
-      }, 15000); // Interval for moving to the next stop (in milliseconds) including the wait time at each stop
+        }, 30);
+      }, 15000);
 
       return () => clearInterval(interval);
     }
   }, [busPositionIndex, busStops]);
 
-  // Function to calculate the distance between two points using Haversine formula
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
-    const R = 6371e3; // Earth radius in meters
-    const φ1 = (lat1 * Math.PI) / 180; // Latitude of point 1 in radians
-    const φ2 = (lat2 * Math.PI) / 180; // Latitude of point 2 in radians
-    const Δφ = ((lat2 - lat1) * Math.PI) / 180; // Difference in latitudes
-    const Δλ = ((lon2 - lon1) * Math.PI) / 180; // Difference in longitudes
+    const R = 6371e3;
+    const φ1 = (lat1 * Math.PI) / 180;
+    const φ2 = (lat2 * Math.PI) / 180;
+    const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+    const Δλ = ((lon2 - lon1) * Math.PI) / 180;
 
     const a =
       Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
       Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    const distance = R * c; // Distance in meters
+    const distance = R * c;
     return distance;
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.navBar}>
-        <TouchableOpacity onPress={toggleStudentDataVisibility}>
+        <TouchableOpacity onPress={toggleFacultyDataVisibility}>
           <FontAwesomeIcon icon={faUser} style={styles.navIcon} />
         </TouchableOpacity>
       </View>
@@ -229,7 +210,6 @@ const FacultyInfoScreen = () => {
           {busStops.map((stop, index) => (
             <Marker key={index} title={stop.name} coordinate={stop} pinColor={index === 0 ? 'blue' : 'red'} />
           ))}
-          {/* Display bus marker at the current bus position */}
           {busPosition && (
             <Marker
               title="Bus"
@@ -285,7 +265,6 @@ const styles = StyleSheet.create({
     width: '100%',
     aspectRatio: 1,
     height: 340,
-    // Adjust the height as needed
   },
   stepper: {
     flexDirection: 'column',
@@ -308,19 +287,19 @@ const styles = StyleSheet.create({
   },
   userDataContainer: {
     position: 'absolute',
-    top: 90, // Adjust the top position as needed
+    top: 90,
     left: 10,
     right: 10,
-    backgroundColor: '#FCE5CD', // Light orange background color
+    backgroundColor: '#FCE5CD',
     padding: 20,
     borderRadius: 10,
     borderWidth: 2,
-    borderColor: '#F7965C', // Border color
+    borderColor: '#F7965C',
   },
   userDataText: {
     marginBottom: 10,
     fontSize: 16,
-    color: '#5E2612', // Dark brown text color
+    color: '#5E2612',
   },
 });
 
